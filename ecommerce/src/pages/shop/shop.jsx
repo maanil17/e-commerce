ffce0bc1b9447679e127fe0f800da14f349a72a5
maanil17 <mlaad17@gmail.com>
2022-12-22 03:22:06 +0530

    final changes made

---
+++ ecommerce/src/pages/shop/shop.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { PRODUCTS } from '../../products'
+import { Product } from './product'
+import './shop.css'
+
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+         <div className='shopTitle'>
+            <h1>Ecommerce Store</h1>
+         </div>
+
+
+         <div className='products'>
+            {PRODUCTS.map((product) =>{
+                return (
+                    <Product data={product}/>
+                )
+            })}
+
+         </div>
+    </div>
+  )
+}
+
